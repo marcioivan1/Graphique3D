3f2eef37534b76d7b2921d0159453394a4b4843f
Marcio Ivan <marciofilho96@yahoo.com.br>
2019-04-17 22:58:01 +0200

    implement little rotation on axis x and y to projetile
    still need some fixes i believe

--- node.py
+++ node.py
@@ -1,5 +1,6 @@
 from transform import Trackball, identity, translate, rotate
 from transform import scale, lerp, vec
+import glfw
 
 class Node:
     """ Scene graph transform and parameter broadcast node """
@@ -17,7 +18,7 @@ class Node:
         """ Recursive draw, passing down named parameters & model matrix. """
         # merge named parameters given at initialization with those given here
         param = dict(param, **self.param)
-        model2 = model @ self.transform 
+        model2 = model @ self.transform
         pos = model2@vec(0,0,0,1)
         self.position = pos[:3]
         for child in self.children:
@@ -29,4 +30,19 @@ class Node:
     def get_position(self):
         return self.position
 
+# ------------  Rotation control class ----------------------------------------
+class RotationControlNode(Node):
+    def __init__(self, key_up, key_down, axis, angle=0, **param):
+        super().__init__(**param)
+        self.angle, self.axis = angle, axis
+        self.key_up, self.key_down = key_up, key_down
 
+    def draw(self, projection, view, model, win=None, **param):
+        assert win is not None
+        self.angle += 0.2 * int(glfw.get_key(win, self.key_up) == glfw.PRESS)
+        self.angle -= 0.2 * int(glfw.get_key(win, self.key_down) == glfw.PRESS)
+        #model = model @ param.get('transf', identity())
+        self.transform = rotate(self.axis, self.angle)
+
+        # call Node's draw method to pursue the hierarchical tree calling
+        super().draw(projection, view, model, win=win, **param)
